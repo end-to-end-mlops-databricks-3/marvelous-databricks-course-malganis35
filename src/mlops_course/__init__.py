d6001e9b0cf60930aeae1b81492ca8646fe2810a
Cao Tri DO <cao-tri.do@keyrus.com>
2025-05-13 23:39:48 +0200

    refactor: precommit init

--- src/mlops_course/__init__.py
+++ src/mlops_course/__init__.py
@@ -1,4 +1,4 @@
-""" hotel_resa package."""
+"""hotel_resa package."""
 
 import importlib.metadata
 import importlib.resources
@@ -7,10 +7,13 @@ from pathlib import Path
 THIS_DIR = Path(__file__).parent
 PROJECT_DIR = (THIS_DIR / "../..").resolve()
 
+
 def get_version() -> str:
     """Retrieve the version of the package.
+
     This function attempts to read the version from the installed package,
     and if not found, reads it from a version.txt file.
+
     :return: The version string of the package.
     """
     try:
@@ -20,5 +23,6 @@ def get_version() -> str:
         # If not installed, read from the version.txt file
         with importlib.resources.files(__package__).joinpath("../../version.txt").open("r", encoding="utf-8") as file:
             return file.read().strip()
+
 
 __version__ = get_version()
