70c68b521e1b16a9ff8378b9c5c77021dd40ad06
Cao Tri DO <cao-tri.do@keyrus.com>
2025-05-13 23:41:21 +0200

    refactor: return precommit

--- src/mlops_course/utils/timer.py
+++ src/mlops_course/utils/timer.py
@@ -1,17 +1,23 @@
 import time
+from collections.abc import Callable
 from contextlib import contextmanager
 from functools import wraps
+from typing import ContextManager, ParamSpec, TypeVar
+
 from loguru import logger
+
+P = ParamSpec("P")
+R = TypeVar("R")
 
 
 @contextmanager
-def measure_time(task_name=""):
-    """
-    A context manager to measure the execution time of a code block.
+def measure_time(task_name: str = "") -> ContextManager[None]:
+    """Measure the execution time of a code block.
 
     :param task_name: A descriptive name for the task being measured.
     :type task_name: str
     :yield: Executes the block of code wrapped by the context manager.
+
     :example:
 
         with measure_time("Training model"):
@@ -26,29 +32,20 @@ def measure_time(task_name=""):
         print(f"{task_name} took {execution_time:.2f} seconds to execute.")
 
 
-def timeit(func):
-    """
-    A decorator to measure and log the execution time of a function.
+def timeit(func: Callable[P, R]) -> Callable[P, R]:
+    """A decorator to measure and log the execution time of a function.
 
     :param func: The function whose execution time is to be measured.
-    :type func: callable
-    :return: A wrapped function that logs its execution duration via loguru.
-    :rtype: callable
-    :example:
-
-        @timeit
-        def process_data():
-            ...
-
-    The log output will be of the format:
-        "Function process_data Took 0.1234 seconds"
+    :return: A wrapped function that logs its execution duration.
     """
+
     @wraps(func)
-    def timeit_wrapper(*args, **kwargs):
+    def timeit_wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
         start_time = time.perf_counter()
         result = func(*args, **kwargs)
         end_time = time.perf_counter()
         total_time = end_time - start_time
-        logger.info(f'Function {func.__name__} Took {total_time:.4f} seconds')
+        logger.info(f"Function {func.__name__} Took {total_time:.4f} seconds")
         return result
+
     return timeit_wrapper
