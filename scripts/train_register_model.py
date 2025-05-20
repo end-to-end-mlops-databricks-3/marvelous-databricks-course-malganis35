d046ff855dd5322a0c9afaabdf56ae2339f5608b
Cao Tri DO <cao-tri.do@keyrus.com>
2025-05-20 23:02:58 +0200

    feat: implement BasicModel class for hotel reservation prediction and integrate MLflow logging

---
+++ scripts/train_register_model.py
@@ -0,0 +1,49 @@
+# Databricks notebook source
+
+import os
+import mlflow
+from loguru import logger
+from pyspark.sql import SparkSession
+
+from mlops_course.utils.config import ProjectConfig, Tags
+from mlops_course.model.basic_model import BasicModel
+
+from dotenv import load_dotenv
+from marvelous.common import is_databricks
+
+# COMMAND ----------
+if not is_databricks():
+    load_dotenv()
+    profile = os.environ["PROFILE"]
+    mlflow.set_tracking_uri(f"databricks://{profile}")
+    mlflow.set_registry_uri(f"databricks-uc://{profile}")
+
+# COMMAND ----------
+config = ProjectConfig.from_yaml(config_path="../project_config.yml", env="dev")
+spark = SparkSession.builder.getOrCreate()
+tags_dict = {"git_sha": "abcd12345", "branch": "week2", "job_run_id": ""}
+tags = Tags(**tags_dict)
+
+# COMMAND ----------
+# Initialize model
+# Initialize model with the config path
+basic_model = BasicModel(config=config, tags=tags, spark=spark)
+logger.info("Model initialized.")
+
+# COMMAND ----------
+# Load data and prepare features
+basic_model.load_data()
+basic_model.prepare_features()
+logger.info("Loaded data, prepared features.")
+
+# COMMAND ----------
+# Train + log the model (runs everything including MLflow logging)
+basic_model.train()
+basic_model.log_model()
+logger.info("Model training completed.")
+
+# COMMAND ----------
+basic_model.register_model()
+logger.info("Registered model")
+
+# COMMAND ----------
