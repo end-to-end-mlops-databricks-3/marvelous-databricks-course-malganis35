213fd56e85d9499847d7eae73e49ed31941e0d8b
Cao Tri DO <cao-tri.do@keyrus.com>
2025-09-25 12:17:40 +0200

    refactor: consolidate configuration variable definitions

--- scripts/process_data.py
+++ scripts/process_data.py
@@ -11,11 +11,13 @@ from mlops_course.feature.data_processor import DataProcessor
 from mlops_course.utils.config import ProjectConfig
 
 # COMMAND ----------
+CONFIG_FILE = "../project_config.yml"
+ENVIRONMENT_CHOICE="dev"
+
+# COMMAND ----------
 
 logger.info("Load configuration from YAML file")
-config_path = "../project_config.yml"
-
-config = ProjectConfig.from_yaml(config_path=config_path, env="dev")
+config = ProjectConfig.from_yaml(config_path=CONFIG_FILE, env=ENVIRONMENT_CHOICE)
 
 logger.info("Configuration loaded:")
 logger.info(yaml.dump(config, default_flow_style=False))
